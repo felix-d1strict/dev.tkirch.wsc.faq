ea3403da9d15ebf85c95cea0fdf9a6b41e258780
Peter Lohse <hanashi@hanashi.eu>
2025-07-03 19:51:21 +0200

    replace fetch with render

--- files/lib/system/endpoint/controller/hanashi/questions/search/RenderSearch.class.php
+++ files/lib/system/endpoint/controller/hanashi/questions/search/RenderSearch.class.php
@@ -17,7 +17,7 @@ final class RenderSearch implements IController
     public function __invoke(ServerRequestInterface $request, array $variables): ResponseInterface
     {
         return new JsonResponse([
-            'template' => WCF::getTPL()->fetch('shared_faqQuestionSearchDialog', 'wcf'),
+            'template' => WCF::getTPL()->render('wcf', 'shared_faqQuestionSearchDialog', []),
         ]);
     }
 }
